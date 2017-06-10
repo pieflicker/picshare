a153edd27df000bde844c840e9010dedaaf63e29
pieflicker <pieflicker@gmail.com>
2017-06-10 18:05:39 -0400

    adding users, and more layout

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,32 @@
+class UserController < ApplicationController
+  
+  def new
+  	@user = User.new
+  end
+
+  def create
+
+  	@user = User.new(user_params)
+
+  	if @user.save
+  		session[:user_id] = @user_id
+  		redirect_to '/'
+  	else
+  		flash[:notice] = "Something went wrong"
+  		redirect_to '/signup'
+  end
+
+
+private
+
+	def user_params
+		params.require(:user).permit(:email, :first_name, :password, :password_confirmation, :event_pin)
+		
+	end
+
+	def event_pin
+		event_pin = "110317"
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new', to: 'users#new', as: 'signup'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
 root 'welcome#index'
+
 
 end
